30c857ad1ec5fca57586c041491d62efdca3d537
Arwaelsaid <107340819+Arwaelsaid@users.noreply.github.com>
2022-06-12 09:11:35 +0200

    Add files via upload

---
+++ max.php
@@ -0,0 +1,121 @@
+<?php
+#app
+// user -> enter his name
+// user -> enter his phone
+// user -> select $products = ['laptop'=>15000,'mobile'=>12000,'tv'=>10000,'tshirt'=> 250]
+// user -> select  $cities = ['cairo'=>0,'alex'=>50,'others'=>200]
+// $discount = ['laptop'=>0.2,'mobile'=>0.1,'tv'=> 0.05 , 'tshirt'=>0]
+// vat : 14%
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Ecommerce</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center text-primary mt-5">
+                <h4>
+                    max&mini 
+                </h4>
+            </div>
+            <div class="col-4 offset-4  mt-5">
+                <form action="<?PHP $_SERVER['PHP_SELF'];?>" method="POST">
+                    <div class="form-group">
+                        <label for="Name">Num1</label>
+                        <input type="text" name="num1" id="Num1" class="form-control" placeholder="" aria-describedby="helpId">
+                    </div>
+                    
+                
+                    <div class="form-group">
+                        <label for="Name">Num2</label>
+                        <input type="text" name="num2" id="Num2" class="form-control" placeholder="" aria-describedby="helpId">
+                    </div>
+                    
+                
+                    <div class="form-group">
+                        <label for="Name">Num3</label>
+                        <input type="text" name="num3" id="Num3" class="form-control" placeholder="" aria-describedby="helpId">
+                    </div>
+                    <!-- <div class="form-group">
+                        <label for="Phone">Phone</label>
+                        <input type="number" name="phone" id="Phone" class="form-control" placeholder="" aria-describedby="helpId">
+                    </div> -->
+                    <!-- <div class="form-group">
+                        <label for="Product">Product</label>
+                        <select name="product" id="Product" class="form-control">
+                            <option value="laptop">Laptop</option>
+                            <option value="tv">Tv</option>
+                            <option value="mobile">Mobile</option>
+                            <option value="tshirt">Tshirt</option>
+                        </select>
+                    </div> -->
+                    <!-- <div class="form-group">
+                        <label for="City">City</label>
+                        <select name="city" id="City" class="form-control">
+                            <option value="cairo">Cairo</option>
+                            <option value="alex">Alex</option>
+                            <option value="others">Others</option>
+                        </select>
+                    </div> -->
+                    <button class=" btn btn-outline-primary form-control"> Calculate </button>
+                    <?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$num3 = $_POST['num3'];
+
+
+if($num1>=$num2 && $num1>=$num3)
+{
+    echo 'num1 is max='.$num1;
+}
+elseif($num2>=$num1 && $num2>=$num3)
+{
+    echo 'num2 is max='.$num2;
+}
+else
+{
+    echo 'num3 is max='.$num3;
+}
+echo '<br>';
+#######################################################
+if($num1<=$num2 && $num1<=$num3)
+{
+    echo 'num1 is mini='.$num1;
+}
+elseif($num2<=$num1 && $num2<=$num3)
+{
+    echo 'num2 is mini='.$num2;
+}
+else
+{
+    echo 'num3 is mini='.$num3;
+}
+
+?>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
